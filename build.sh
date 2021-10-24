40cecfcb0c9cdb51da8943a22b9444d6b568d128
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-24 17:35:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
-lunch omni_Guamp-eng
+lunch omni_guamp-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
 make -j$(nproc) recoveryimage
