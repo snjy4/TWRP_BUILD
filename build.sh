12f8eb334bf0fc269b3f18403586bfc3326475bd
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-25 06:41:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
 cd $ANDROID_ROOT
 source build/envsetup.sh
+. build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 lunch twrp_guamp-eng
-#mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
-#touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
 mka recoveryimage 
