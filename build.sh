983ad5582b550e373db7fc66681f35c6d611a4e9
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-25 06:39:49 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
-lunch omni_guamp-eng
+lunch twrp_guamp-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
 mka recoveryimage 
