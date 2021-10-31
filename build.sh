8ed4c7d454df2290711c03019f23fe97b705905e
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-31 05:04:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,5 +21,6 @@ export LC_ALL="C"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 source build/envsetup.sh
+. /build/envsetup.sh
 lunch twrp_guamp-eng
 make -j$(nproc --all) recoveryimage
