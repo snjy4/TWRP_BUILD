d37e2ffae291a31d9a590051326f1bfb5a77abfb
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-31 12:23:50 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export TW_THEME=portrait_hdpi
 source build/envsetup.sh
 . build/envsetup.sh
 lunch twrp_guamp-eng
