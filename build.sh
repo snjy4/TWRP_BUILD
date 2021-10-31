d1bd2b2f1511bc995f96c7fd61aa33bf941e37ab
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-31 14:33:38 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,8 @@ source of.env
 sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
 cd $ANDROID_ROOT
-rm -rf Android.bp
-rm -rf bootctrl
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 export TW_THEME=portrait_hdpi
 source build/envsetup.sh
 . build/envsetup.sh
