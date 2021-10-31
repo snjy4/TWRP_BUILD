3cfd294d9ea1b65fac24716fd90bf894a77f347c
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-10-31 06:35:58 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 source of.env
+sudo apt-get install -y python2 python2-dev
+sudo apt install -y make
 sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
 sudo apt-get update
-sudo apt-get install -y python2 python2-dev
-sudo apt install -y make
-cd ~
-sudo apt install git aria2 -y
-git clone https://gitlab.com/OrangeFox/misc/scripts
-cd scripts
-sudo bash setup/android_build_env.sh
-sudo bash setup/install_android_sdk.sh
-mkdir ~/OrangeFox_10
-cd ~/OrangeFox_10
-rsync rsync://sources.orangefox.download/sources/fox_10.0 . --progress -a
-mkdir -p device/motorola/guamp
-git clone https://github.com/mistersmee/android_device_motorola_cebu.git -b android-11 device/motorola/guamp
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+cd $ANDROID_ROOT
 source build/envsetup.sh
 . /build/envsetup.sh
 lunch twrp_guamp-eng
