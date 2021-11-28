9fcd69fff90793f133fddc4b8f43c52b21615eb0
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-11-28 21:35:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
+export ALLOW_MISSING_DEPENDENCIES=true && \
+export LC_ALL="C" && \
+export TW_THEME=portrait_hdpi && \
 source of.env && \
 cd /home/runner/work/twrp/ && \
 . build/envsetup.sh && \
