6553ad3e7344e0441695db325a5a3f1800295e81
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-11-28 19:56:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ source of.env
 sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
 cd $ANDROID_ROOT
-source build/envsetup.sh
-. build/envsetup.sh
+source build/envsetup.sh && . build/envsetup.sh
 lunch twrp_guamp-eng
 mka recoveryimage
