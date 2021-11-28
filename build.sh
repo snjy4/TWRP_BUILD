d6f97cbe9aa5e0dc8ac3d33e457cd3104f76a2e6
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-11-28 19:55:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@ source of.env
 sudo rm /bin/python
 sudo ln -s /bin/python2 /bin/python
 cd $ANDROID_ROOT
-export ALLOW_MISSING_DEPENDENCIES=true
-export LC_ALL="C"
-export TW_THEME=portrait_hdpi
 source build/envsetup.sh
 . build/envsetup.sh
 lunch twrp_guamp-eng
