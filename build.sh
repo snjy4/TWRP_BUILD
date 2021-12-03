e6b0924348c1b2fb351260afabae40c8b9b4037f
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-12-03 14:21:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-sudo rm /bin/python
-sudo ln -s /bin/python2 /bin/python
-export ALLOW_MISSING_DEPENDENCIES=true && \
-export LC_ALL="C" && \
-export TW_THEME=portrait_hdpi && \
-source of.env && \
-cd /home/runner/work/twrp/ && \
-. build/envsetup.sh && \
-lunch twrp_guamp-eng && \
+source of.env
+cd $ANDROID_ROOT
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL="C"
+export TW_THEME=portrait_hdpi
+source build/envsetup.sh
+. build/envsetup.sh
+lunch twrp_guamp-eng
 mka recoveryimage
