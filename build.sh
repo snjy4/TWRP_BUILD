1007985bf3a2c94b927b08e034d042b98e02631c
snjy4 <92902636+snjy4@users.noreply.github.com>
2021-12-19 19:13:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source of.env
 cd $ANDROID_ROOT
-export ALLOW_MISSING_DEPENDENCIES=true
-export LC_ALL="C"
-export TW_THEME=portrait_hdpi
 source build/envsetup.sh
 lunch twrp_guamp-eng
 mka recoveryimage
